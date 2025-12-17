36840640e059f0116642ee5af759043473cd6f4c
shri-acha <happy.irhs@gmail.com>
2025-12-17 20:30:14 +0545

    chore: Formatting and modify variable names

--- galileo/src/tile_schema/schema.rs
+++ galileo/src/tile_schema/schema.rs
@@ -52,7 +52,9 @@ impl TileSchema {
         } else {
             None
         }
-    } /// Width of a single tile.
+    }
+
+    /// Width of a single tile.
     pub fn tile_width(&self) -> u32 {
         self.tile_width
     }
